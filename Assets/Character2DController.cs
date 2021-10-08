f1d9164275cec7629d89a4ac434032be601c843e
MikhaelG <mikhael.gerber@elev.ga.lbs.se>
2021-10-08 14:09:47 +0200

    början på movement kod och test scen

---
+++ Assets/Character2DController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Character2DController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+    }
+}
