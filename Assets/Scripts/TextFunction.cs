17e314e823b4b66287556b3e349653004bb3a8a3
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2021-10-08 14:31:22 +0200

    Meny grejsimojs

---
+++ Assets/Scripts/TextFunction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextFunction : MonoBehaviour
+{
+
+    public Text[] text;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //var vertical = Input.GetAxis("Vertical");
+
+    }
+}
