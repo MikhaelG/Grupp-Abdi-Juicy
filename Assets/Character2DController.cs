3643da9c01173f94c1f514f9dae29167dfb7531d
MikhaelG <mikhael.gerber@elev.ga.lbs.se>
2021-10-08 14:22:38 +0200

    Movement - klar

--- Assets/Character2DController.cs
+++ Assets/Character2DController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Character2DController : MonoBehaviour
 {
     public float MovementSpeed = 1;
-    public float JumpForce = 1;
 
     private Rigidbody2D _rigidbody;
 
@@ -14,8 +13,8 @@ public class Character2DController : MonoBehaviour
 
     private void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
-
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+        transform.position += new Vector3(movementX, movementY, 0) * Time.deltaTime * MovementSpeed;
     }
 }
