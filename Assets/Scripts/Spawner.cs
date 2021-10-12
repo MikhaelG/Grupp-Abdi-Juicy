7ab5616bbb5c72141de790826f1800e3b0d8e1c1
AndyPandyA <andreas.larsson2@elev.ga.lbs.se>
2021-10-12 10:38:05 +0200

    EnemyAI
    Har skapat enemy

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Rigidbody2D alien;
+    public Transform spawner;
+    public static int clock = 999;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+        StartCoroutine(Move());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Timer()
+    {
+        for (int i = 0; i < clock; i++)
+        {
+            Instantiate(alien, spawner.position, spawner.rotation);
+            yield return new WaitForSeconds(1);
+            Instantiate(alien, spawner.position, spawner.rotation);
+        }
+    }
+
+    IEnumerator Move()
+    {
+
+        for (int i = 0; i < clock; i++)
+        {
+            spawner.transform.position = new Vector3(Random.Range(15, -15), 5, 0);
+            yield return new WaitForSeconds(1);
+            spawner.transform.position = new Vector3(Random.Range(15, -15), 5, 0);
+        }
+    }
+}
