47b00f612621e1daa644f045662dc2813c06c50f
MikhaelG <mikhael.gerber@elev.ga.lbs.se>
2021-10-12 11:14:01 +0200

    pew pew enemy

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
+    public int damage = 1;
     public Rigidbody2D rb;
+    
     void Start()
     {
         rb.velocity = transform.right * speed;
@@ -13,7 +15,11 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D (Collider2D hitInfo)
     {
-        Debug.Log(hitInfo.name);
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
         Destroy(gameObject);
     }
 
