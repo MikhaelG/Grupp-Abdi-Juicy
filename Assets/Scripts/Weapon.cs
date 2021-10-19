f6fce5c2e0084714c0079c2223da0f3024edb726
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2021-10-19 17:07:11 +0200

    Fixat skjut anim
    Jag bytte sprite på bullet ikonen. Vi måste fixa så att man kan se den

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public Animator playerAnim; //Saga
     public Transform firePoint;
     public GameObject bulletPrefab;
     
@@ -12,11 +13,19 @@ public class Weapon : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+        }else if (Input.GetButtonUp("Fire1"))
+        {
+            //Så att inte animationen fortsätter för evigt och slutar när man tar upp knappen -Saga
+            playerAnim.SetBool("Shoot", false);
+            print("Stop shooting");
         }
     }
 
     void Shoot ()
     {
+        //Gör så att skjut animationen är på när den skjuter objektet -Saga
+        playerAnim.SetBool("Shoot", true);
+        
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
