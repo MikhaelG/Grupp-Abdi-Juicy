64a0daecc4f6839a5407f916f94c003babbe816e
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2021-10-25 20:55:29 +0200

    Försök att fixa meny
    Fråga Tobbe om hjälp imorgon

---
+++ Assets/ButtonAnim.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnim : MonoBehaviour
+{
+    public Animator buttonAnim;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
