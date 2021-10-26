c1ed93dff594d505522b273a4f96def44b216dd1
AndyPandyA <andreas.larsson2@elev.ga.lbs.se>
2021-10-26 09:45:03 +0200

    EnemyMainGame

--- Assets/Scripts/EnemyFall.cs
+++ Assets/Scripts/EnemyFall.cs
@@ -10,7 +10,7 @@ public class EnemyFall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 2);
     }
 
     // Update is called once per frame
